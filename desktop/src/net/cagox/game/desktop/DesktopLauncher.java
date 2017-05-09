2002b3a033447dd5ff898beccf0251377553bb6c
Kenneth Burling (burlingk) <burlingk@cagox.net>
2017-05-10 05:52:19 +0900

    Initial Commit

---
+++ desktop/src/net/cagox/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.cagox.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.cagox.game.MiniTownGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MiniTownGame(), config);
+	}
+}
