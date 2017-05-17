e56ed4b4bd4f0f36ef07ba2470bb5d80d28850fd
Kenneth Burling (burlingk) <burlingk@cagox.net>
2017-05-17 22:35:15 +0900

    Animated the sprite, and made it directional.

--- core/src/net/cagox/game/MiniTownGame.java
+++ core/src/net/cagox/game/MiniTownGame.java
@@ -1,5 +1,8 @@
 package net.cagox.game;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.HashMap;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -10,6 +13,9 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -30,6 +36,11 @@ public class MiniTownGame extends ApplicationAdapter implements InputProcessor {
 	float cellSize;
 	float w, h;
 
+	HashMap<String, Animation<TextureRegion>> pcSprite = new HashMap<String, Animation<TextureRegion>>();
+	Texture pcWalkSheet;
+	String pcWalkDirection;
+	float stateTime;
+
 	@Override
 	public void create () {
 		w = Gdx.graphics.getWidth();
@@ -44,27 +55,44 @@ public class MiniTownGame extends ApplicationAdapter implements InputProcessor {
 		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, gameScale);
 		Gdx.input.setInputProcessor(this);
 
+		//pcSprite ;
+
+		pcWalkSheet = new Texture(Gdx.files.internal("barbarian.png"));
+		TextureRegion[][] tmp = TextureRegion.split(pcWalkSheet, 64, 64);
+
+		pcSprite.put("UP", new Animation<TextureRegion>(0.1f, tmp[0]));
+		pcSprite.put("LEFT", new Animation<TextureRegion>(0.1f, tmp[1]));
+		pcSprite.put("DOWN", new Animation<TextureRegion>(0.1f, tmp[2]));
+		pcSprite.put("RIGHT", new Animation<TextureRegion>(0.1f, tmp[3]));
+
+
 		sb = new SpriteBatch();
-		texture = new Texture(Gdx.files.internal("sprite.png"));
-		sprite = new Sprite(texture);
-		sprite.setScale(gameScale);
+		stateTime = 0f;
+
+		pcWalkDirection = "RIGHT";
+
+		//texture = new Texture(Gdx.files.internal("sprite.png"));
+		//sprite = new Sprite(texture);
+		//sprite.setScale(gameScale);
 
 
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0.5f, 0, 0, 1);
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.7f, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stateTime += Gdx.graphics.getDeltaTime();
+
 		camera.update();
 		tiledMapRenderer.setView(camera);
 		tiledMapRenderer.render();
 
-		sprite.setX(w/2);
-		sprite.setY(w/2);
+		Animation<TextureRegion> walkAnimation = pcSprite.get(pcWalkDirection);
+		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
 		sb.begin();
-		sprite.draw(sb);
+		sb.draw(currentFrame, w/2, h/2);
 		sb.end();
 
 	}
@@ -86,14 +114,22 @@ public class MiniTownGame extends ApplicationAdapter implements InputProcessor {
 
 	@Override
 	public boolean keyUp(int keycode) {  //TODO:  Figure out how to do movement from touchDown.
-		if(keycode == Input.Keys.LEFT)
+		if(keycode == Input.Keys.LEFT) {
+			pcWalkDirection = "LEFT";
 			camera.translate((-32*gameScale),0);
-		if(keycode == Input.Keys.RIGHT)
+		}
+		if(keycode == Input.Keys.RIGHT) {
+			pcWalkDirection = "RIGHT";
 			camera.translate((32*gameScale),0);
-		if(keycode == Input.Keys.UP)
-			camera.translate(0,(32*gameScale));
-		if(keycode == Input.Keys.DOWN)
+		}
+		if(keycode == Input.Keys.UP) {
+			pcWalkDirection = "UP";
+			camera.translate(0, (32 * gameScale));
+		}
+		if(keycode == Input.Keys.DOWN) {
+			pcWalkDirection = "DOWN";
 			camera.translate(0,(-32*gameScale));
+		}
 		if(keycode == Input.Keys.NUM_1)
 			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
 		if(keycode == Input.Keys.NUM_2)
@@ -134,3 +170,8 @@ public class MiniTownGame extends ApplicationAdapter implements InputProcessor {
 		return false;
 	}
 }
+
+
+
+//TODO: https://github.com/libgdx/libgdx/wiki/2D-Animation shows how to handle animation of the sprite.
+
